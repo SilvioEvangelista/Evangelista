318b9fa8db9f6ea2bd76c3ebb0a39e101da28ca3
SilvioEvangelista <silviosqldba@gmail.com>
2025-02-02 16:12:40 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+let amigos = [];
+
+function adicionarAmigo() {
+    let amigo = document.getElementById("amigo");
+    let regex = /^[a-zA-Z]+$/; 
+    if (amigo.value == "") {
+        alert("Por favor, insira um nome.");
+        amigo.focus();
+        return;
+    }
+    if (!regex.test(amigo.value)) {
+        alert("Por favor, insira um nome válido (sem caracteres especiais).");
+        amigo.focus();
+        return;
+    }
+    if (amigos.includes(amigo.value)) {//amigo ja ta incluso?
+        alert('Nome já existe!');
+        return; 
+    }
+    if (amigo.value.length < 3) {
+        alert("O nome deve ter pelo menos três letras.");
+        amigo.focus();
+        return;
+    }
+    
+
+    amigos.push(amigo.value); // Adiciona o amigo ao array
+    alert('Amigo adicionado com sucesso!');
+    atualizarListaAmigos(); // Atualiza a lista de amigos exibida
+
+    amigo.value = ""; // limpa o campo do amigo
+    amigo.focus(); // Adiciona o foco de volta ao campo de entrada
+}
+
+function atualizarListaAmigos() {
+    let listaAmigos = document.getElementById("listaAmigos");
+
+    // Limpar a lista existente
+    listaAmigos.innerHTML = "";
+
+    // Percorrer o array amigos e adicionar cada nome como um elemento <li>
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+    }
+}
+function sortearAmigo(){
+    let resultado = document.getElementById("resultado"); 
+    
+    // Validar que há amigos disponíveis
+    if (amigos.length === 0) { 
+        resultado.innerHTML = "Nenhum amigo disponível para sortear.";
+        return; 
+    }
+    // Gerar um índice aleatório
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length); 
+    // Obter o nome sorteado 
+    let amigoSorteado = amigos[indiceAleatorio];
+    // Mostrar o resultado 
+    resultado.innerHTML = "Amigo sorteado: " + amigoSorteado;
+}
+
+
